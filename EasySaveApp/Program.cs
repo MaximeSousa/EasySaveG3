3c26658fb2616ba7c641a36cfa2804ee4e5baf3b
Maxime SOUSA <maxime.sousa@viacesi.fr>
2024-01-31 10:59:49 +0100

    Ajoutez des fichiers projet.

---
+++ EasySaveApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasySaveApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EasySave !");
+        }
+    }
+}
