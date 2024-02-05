c70fda290086c7e166aef99cc4889f518a1f70bb
AveeXe <antoine.boucly@viacesi.fr>
2024-02-05 15:22:27 +0100

    Create log.cs

---
+++ EasySaveApp/Models/log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveApp.Models
+{
+    public class Log
+    {
+        public string FileName { get; set; }
+        public string FileSource { get; set; }
+        public string FileTarget { get; set; }
+        public string FileSize { get; set; }
+        public string FileTransferTime { get; set; }
+        public string FileTime { get; set; }
+
+    }
+}
