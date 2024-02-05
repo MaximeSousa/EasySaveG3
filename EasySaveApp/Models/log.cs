89f89e5c3685abbaec915c80738015f22bff5488
AveeXe <antoine.boucly@viacesi.fr>
2024-02-05 15:22:27 +0100

    Create log.cs

---
+++ EasySaveApp/Models/log.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveApp.Models
+{
+    public class Log
+    {
+        public string FileName { get; set; }
+        public string FileSource { get; set; }
+        public string FileTarget { get; set; }
+        public string FileSize { get; set; }
+        public string FileTransferTime { get; set; }
+        public string FileTime { get; set; }
+
+    }
+
+    public class ExcuteLog
+    {
+        public static void Main()
+        {
+            var Log = new Log
+            {
+                FileName = 123
+                FileSource = 123
+                FileTarget = 123
+                FileSize = 123
+                FileTransferTime = 123
+                FileTime = 123
+            };
+
+            string jsonString = JsonSerializer.Serialize(Log);
+
+            Console.WriteLine(jsonString);
+        }
+    }
+}
--- EasySaveApp/Program.cs
+++ EasySaveApp/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using EasySaveApp.Models.log;
 namespace EasySaveApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to EasySave !");
+            Console.WriteLine("Welcome to EasySave !"); 
+            
         }
     }
 }
