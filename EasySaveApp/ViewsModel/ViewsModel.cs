11ecb0da9adb900a61e5dd5af14ae6400a06bab2
AveeXe <antoine.boucly@viacesi.fr>
2024-02-09 15:25:15 +0100

    MAJ LOG

--- EasySaveApp/ViewsModel/ViewsModel.cs
+++ EasySaveApp/ViewsModel/ViewsModel.cs
@@ -240,6 +240,7 @@ namespace EasySaveApp.ViewsModel
                 BackupFile.SaveBackupsToFile();
 
                 Console.WriteLine($"Backup '{nameBackupChange}' updated successfully.");
+                CreateLog(newName, newSource, newTarget, backup.FileSize, backup.FileTransferTime);
             }
             else
             {
@@ -258,6 +259,7 @@ namespace EasySaveApp.ViewsModel
                 BackupFile.backups.Remove(backup);
                 BackupFile.SaveBackupsToFile();
                 Console.WriteLine($"Backup '{nameBackupDelete}' deleted successfully.");
+                CreateLog(backup.FileName, backup.FileSource, backup.FileTarget, backup.FileSize, backup.FileTransferTime);
             }
             else
             {
