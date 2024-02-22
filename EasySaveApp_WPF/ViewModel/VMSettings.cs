692a6ac7cd19e584ef3fd02f99d89279858cce5f
Maxime SOUSA <maxime.sousa@viacesi.fr>
2024-02-22 11:39:02 +0100

    BugFix

--- EasySaveApp_WPF/ViewModel/VMSettings.cs
+++ EasySaveApp_WPF/ViewModel/VMSettings.cs
@@ -14,57 +14,6 @@ namespace EasySaveApp_WPF.ViewModel
     {
         public VMSettings()
         {
-        private ResourceManager _resourceManager;
-        private CultureInfo _currentCulture;
-
-        private static readonly string METIER_SOFTWARE = "CalculatorApp"; // Logiciel métier
-
-        public ICommand ChangeLanguageToFrenchCommand { get; }
-        public ICommand ChangeLanguageToEnglishCommand { get; }
-        public ICommand BrowseMetierSoftwareCommand { get; } // Ajout de la commande de navigation
-        public ICommand SelectFormat { get; }
-        private string _metierSoftwarePath; // Ajout de la propriété de stockage du chemin du logiciel métier
-        public string MetierSoftwarePath
-        {
-            get { return _metierSoftwarePath; }
-            set
-            {
-                _metierSoftwarePath = value;
-                OnPropertyChanged(nameof(MetierSoftwarePath));
-            }
-        }
-
-        public object LocalizedStrings { get; private set; }
-
-        public VMSettings()
-        {
-            ChangeLanguageToFrenchCommand = new RelayCommand(ChangeLanguageToFrench);
-            ChangeLanguageToEnglishCommand = new RelayCommand(ChangeLanguageToEnglish);
-            BrowseMetierSoftwareCommand = new RelayCommand(BrowseMetierSoftware); // Initialisation de la commande de navigation
-            SelectFormat = new RelayCommand(ConfirmFormat, CanConfirmFormat);
-
-            // Initialisation du gestionnaire de ressources pour les langues
-            _resourceManager = new ResourceManager("EasySaveApp_WPF.Resources.Lang.Resources", Assembly.GetExecutingAssembly());
-            _currentCulture = CultureInfo.CurrentUICulture;
-        }
-
-        private void ChangeLanguageToFrench(object obj)
-        {
-            ChangeCulture("fr");
-        }
-
-        private void ChangeLanguageToEnglish(object obj)
-        {
-            ChangeCulture("en");
-        }
-
-        private void ChangeCulture(string cultureName)
-        {
-            if (_currentCulture.Name != cultureName)
-            {
-                _currentCulture = new CultureInfo(cultureName);
-                CultureInfo.DefaultThreadCurrentCulture = _currentCulture;
-                CultureInfo.DefaultThreadCurrentUICulture = _currentCulture;
 
         }
 
