a9bea94e7f929f9a6d885173c0a656b35ab7936e
Maxime SOUSA <maxime.sousa@viacesi.fr>
2024-02-25 21:23:25 +0100

    Console WPF Socket v1. Mono-instance, FIX

--- EasySaveApp_WPF/App.xaml.cs
+++ EasySaveApp_WPF/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading;
+using System.Windows;
 
 namespace EasySaveApp_WPF
 {
@@ -7,5 +9,33 @@ namespace EasySaveApp_WPF
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            const string mutexName = "AppInstance";
+
+            try
+            {
+                _mutex = new Mutex(true, mutexName, out bool AppInstance);
+                if (!AppInstance)
+                {
+                    MessageBox.Show("Application déja lancée");
+                    Shutdown();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erreur lors de la vérification de l'instance unique de l'application : {ex.Message}");
+                Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            _mutex?.ReleaseMutex();
+            _mutex?.Dispose();
+        }
     }
 }
